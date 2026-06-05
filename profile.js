9de1fb6cd65e3546737c84f1c827e7801c3065f7
lahovvlad07-cell <lahovvlad07@gmail.com>
2026-06-05 23:17:48 +0300

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,4 @@
+// profile.js
+window.renderProfile = async () => {
+    document.getElementById('app').innerHTML = '<div class="card"><h2>👤 Профиль</h2><p>Функционал в разработке. Скоро здесь появятся достижения, аватарка и статистика.</p></div>';
+};
